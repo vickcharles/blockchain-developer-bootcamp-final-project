5a4aa9a5faa9573ab8db2fb87fb1d14a7bb393c0
Vickler <charlesvikler@gmail.com>
2021-11-09 00:26:04 +0100

    minors editions

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import RealEstatePropertyFactory from "./contracts/RealEstatePropertyFactory.json";
 import getWeb3 from "./getWeb3";
 
 import "./App.css";
@@ -17,9 +17,9 @@ class App extends Component {
 
       // Get the contract instance.
       const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
+      const deployedNetwork = RealEstatePropertyFactory.networks[networkId];
       const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
+        RealEstatePropertyFactory.abi,
         deployedNetwork && deployedNetwork.address,
       );
 
@@ -39,7 +39,7 @@ class App extends Component {
     const { accounts, contract } = this.state;
 
     // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
+    await contract.methods.createRealEstateProperty('ds', 'sdsd', 1, 1).send({ from: accounts[0] });
 
     // Get the value from the contract to prove it worked.
     const response = await contract.methods.get().call();
--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,8 @@
+// const HDWalletProvider = require('./truffle-hdwallet-provider');
+
+
+// const mnemonic = 'demise mouse tobacco else balcony protect sad charge crunch powder express swamp water cargo employ';
+
 /**
  * Use this file to configure your truffle project. It's seeded with some
  * common settings for different networks and features like migrations,
@@ -46,6 +51,7 @@ module.exports = {
       port: 7545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
     },
+
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
@@ -58,12 +64,12 @@ module.exports = {
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
     // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    //   provider: new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/v3/52abd812dbbc433b8926f937b90c42eb'),
+    //   network_id: 3
+    //   // gas: 5500000,        // Ropsten has a lower block limit than mainnet
+    //   // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+    //   // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+    //   // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     // },
     // Useful for private networks
     // private: {
