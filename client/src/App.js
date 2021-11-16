c15ed2051c82332991f52cae411cbd8db16691e0
Vickler <charlesvikler@gmail.com>
2021-11-16 10:58:05 +0100

    ui changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,59 +1,23 @@
-import React, { Component } from "react";
-import RealEstatePropertyFactory from "./contracts/RealEstatePropertyFactory.json";
-import getWeb3 from "./getWeb3";
-import RentersList from "./components/containers/renters/RenterList";
+import React, { useEffect } from "react";
+import Properties from "./components/containers/properties/Properties";
 import AppNavBar from "./components/ui/NavBar/AppNavBar";
-
 import "./App.css";
+import { getUserAddresses } from "./redux/AccountSlice";
+import { useDispatch } from "react-redux";
 
-class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+const App = () => {
+  const dispatch = useDispatch();
 
-  componentDidMount = async () => {
-    try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
+  useEffect(() => {
+    dispatch(getUserAddresses());
+  }, [dispatch]);
 
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
-
-      // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      console.log(RealEstatePropertyFactory.networks);
-      const deployedNetwork = RealEstatePropertyFactory.networks[networkId];
-      console.log(deployedNetwork);
-      const instance = new web3.eth.Contract(
-        RealEstatePropertyFactory.abi,
-        deployedNetwork && deployedNetwork.address
-      );
-
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`
-      );
-      console.error(error);
-    }
-  };
-
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
-    // Update state with the result.
-    this.setState({ storageValue: accounts });
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <AppNavBar />
-        <RentersList />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <AppNavBar />
+      <Properties />
+    </div>
+  );
+};
 
 export default App;
--- client/src/components/ui/NavBar/AppNavBar.jsx
+++ client/src/components/ui/NavBar/AppNavBar.jsx
@@ -1,6 +1,8 @@
-/* This example requires Tailwind CSS v2.0+ */
-import { Disclosure } from "@headlessui/react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import { Fragment } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setAccount } from "../../../redux/AccountSlice";
 
 const navigation = [
   { name: "Explorar", href: "#", current: true },
@@ -10,112 +12,121 @@ const navigation = [
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
-export default function AppNavBar() {
-  return (
-    <>
-      <div className="mb-10">
-        <Disclosure as="nav" className="bg-white-800">
-          {({ open }) => (
-            <>
-              <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="flex items-center justify-between h-16">
-                  <div className="flex items-center">
-                    <div className="flex-shrink-0">
-                      <img
-                        className="h-8 w-8"
-                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                        alt="Workflow"
-                      />
-                    </div>
-                    <div className="hidden md:block">
-                      <div className="ml-10 flex items-baseline space-x-4">
-                        {navigation.map((item) => (
-                          <a
-                            key={item.name}
-                            href={item.href}
-                            className={classNames(
-                              item.current
-                                ? "text-gray-800"
-                                : "text-gray-400 hover:bg-gray-100",
-                              "px-3 py-2 rounded-md text-sm font-semibold"
-                            )}
-                            aria-current={item.current ? "page" : undefined}
-                          >
-                            {item.name}
-                          </a>
-                        ))}
-                      </div>
-                    </div>
-                  </div>
-                  <div className="hidden md:block">
-                    <div className="ml-4 flex items-center md:ml-6">
-                      <div className="ml-10 flex items-baseline space-x-4">
-                        <a
-                          className={classNames(
-                            "bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
-                          )}
-                          aria-current={"page"}
-                        >
-                          Connect to a Wallet
-                        </a>
-                      </div>
 
-                      {/* <Menu as="div" className="ml-3 relative">
-                        <div>
-                          <Menu.Button className="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                            <span className="sr-only">Open user menu</span>
-                            <img className="h-8 w-8 rounded-full" src={user.imageUrl} alt="" />
-                          </Menu.Button>
-                        </div>
-                        <Transition
-                          as={Fragment}
-                          enter="transition ease-out duration-100"
-                          enterFrom="transform opacity-0 scale-95"
-                          enterTo="transform opacity-100 scale-100"
-                          leave="transition ease-in duration-75"
-                          leaveFrom="transform opacity-100 scale-100"
-                          leaveTo="transform opacity-0 scale-95"
-                        >
-                          <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                            {userNavigation.map((item) => (
-                              <Menu.Item key={item.name}>
-                                {({ active }) => (
-                                  <a
-                                    href={item.href}
-                                    className={classNames(
-                                      active ? 'bg-gray-100' : '',
-                                      'block px-4 py-2 text-sm text-gray-700'
-                                    )}
-                                  >
-                                    {item.name}
-                                  </a>
-                                )}
-                              </Menu.Item>
-                            ))}
-                          </Menu.Items>
-                        </Transition>
-                      </Menu> */}
-                    </div>
-                  </div>
-                  <div className="-mr-2 flex md:hidden ">
-                    <Disclosure.Button className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                      <span className="sr-only">Open main menu</span>
-                      {open ? (
-                        <XIcon className="block h-6 w-6" aria-hidden="true" />
-                      ) : (
-                        <MenuIcon
-                          className="block h-6 w-6"
-                          aria-hidden="true"
-                        />
-                      )}
-                    </Disclosure.Button>
+export default function AppNavBar() {
+  const count = useSelector((state) => state.account.address);
+  const dispatch = useDispatch();
+
+  const handleWalletConnection = async () => {
+    if (typeof window.ethereum !== "undefined") {
+      window.ethereum
+        .request({ method: "eth_requestAccounts" })
+        .then((res) => {
+          dispatch(setAccount(res));
+        })
+        .catch((e) => {
+          console.log(e.toString());
+        });
+    }
+  };
+
+  return (
+    <Disclosure as="nav" className="bg-white-800 mb-10">
+      {({ open }) => (
+        <>
+          <div className="max-w-7xl mx-auto">
+            <div className="flex justify-between  mt-5">
+              <div className="flex">
+                <div className="flex-shrink-0"></div>
+                <div className="hidden md:block">
+                  <div className="flex items-baseline space-x-4">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current
+                            ? "text-gray-800"
+                            : "text-gray-400 hover:bg-gray-100",
+                          "px-3 py-2 rounded-md text-sm font-semibold"
+                        )}
+                        aria-current={item.current ? "page" : undefined}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
                   </div>
                 </div>
               </div>
-            </>
-          )}
-        </Disclosure>
-      </div>
-    </>
+
+              <div className="hidden md:block">
+                <div className="ml-4 flex">
+                  {count === "" ? (
+                    <div className="ml-10 flex items-baseline space-x-4">
+                      <button
+                        onClick={handleWalletConnection}
+                        className={classNames(
+                          "bg-gray-900 text-white px-3 py-2 space-x-4 rounded-md text-sm font-medium"
+                        )}
+                      >
+                        Connect to a Wallet
+                      </button>
+                    </div>
+                  ) : (
+                    <Menu as="div" className="ml-3 relative">
+                      <Menu.Button>
+                        <div className="flex  bg-white items-center rounded-full">
+                          <div className="ml-5 mr-1">
+                            <img
+                              alt=""
+                              src="https://docs.metamask.io/metamask-fox.svg"
+                              className="h-5 w-5"
+                            />
+                          </div>
+                          <p className="p-2 font-normal text-gray-800  pr-5">
+                            Account: {count?.slice(0, 4)}
+                          </p>
+                        </div>
+                      </Menu.Button>
+
+                      <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-100"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                      >
+                        <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                          <Menu.Item key="k">
+                            <a
+                              href=""
+                              className="block px-4 py-2 text-sm text-gray-700"
+                            >
+                              "ksjd"
+                            </a>
+                          </Menu.Item>
+                        </Menu.Items>
+                      </Transition>
+                    </Menu>
+                  )}
+                </div>
+              </div>
+              <div className="-mr-2 flex md:hidden ">
+                <Disclosure.Button className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </Disclosure>
   );
 }
