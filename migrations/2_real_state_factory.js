effa9dd107c7a3380356e33204345528dd39ed59
Vickler <charlesvikler@gmail.com>
2021-11-16 10:58:30 +0100

    ui changes

--- migrations/2_real_state_factory.js
+++ migrations/2_real_state_factory.js
@@ -1,5 +1,5 @@
 var Migrations = artifacts.require("./RealEstatePropertyFactory.sol");
 
 module.exports = function (deployer) {
-    deployer.deploy(Migrations);
+  deployer.deploy(Migrations);
 };
