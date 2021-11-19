fd661f04b34b7853c1840b94ec13cc9e6a2abc22
Vickler <charlesvikler@gmail.com>
2021-11-19 14:45:51 +0100

    SM: Add unit testing

--- migrations/2_real_state_factory.js
+++ migrations/2_real_state_factory.js
@@ -1,5 +1,5 @@
-var Migrations = artifacts.require("./RealEstatePropertyFactory.sol");
+var RentProperty = artifacts.require("./RentProperty.sol");
 
 module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(RentProperty);
 };
--- test/simplestorage.js
+++ test/simplestorage.js
@@ -1,15 +1,141 @@
-const SimpleStorage = artifacts.require("./SimpleStorage.sol");
+const RentProperty = artifacts.require("../RentProperty.sol");
 
-contract("SimpleStorage", accounts => {
-  it("...should store the value 89.", async () => {
-    const simpleStorageInstance = await SimpleStorage.deployed();
+contract("RentProperty", (accounts) => {
+  it("...should create a new lessor with 2 properties.", async () => {
+    const simpleStorageInstance = await RentProperty.deployed();
 
-    // Set value of 89
-    await simpleStorageInstance.set(89, { from: accounts[0] });
+    await simpleStorageInstance.createProperty(
+      "foo title",
+      "foo description",
+      200,
+      200
+    );
 
-    // Get stored value
-    const storedData = await simpleStorageInstance.get.call();
+    await simpleStorageInstance.createProperty(
+      "foo title",
+      "foo description",
+      200,
+      200
+    );
 
-    assert.equal(storedData, 89, "The value 89 was not stored.");
+    const numOfPropertiesExpected = 2;
+    const numOfLessorExpected = 1;
+
+    const lessors = await simpleStorageInstance.getLessors.call();
+
+    assert.equal(
+      lessors.length,
+      numOfLessorExpected,
+      "The value 1 was not stored."
+    );
+    assert.equal(
+      lessors[0].properties.length,
+      numOfPropertiesExpected,
+      "The value 2 was not stored."
+    );
+  });
+
+  it("...should create multiple lessors", async () => {
+    const simpleStorageInstance = await RentProperty.deployed();
+
+    await simpleStorageInstance.createProperty(
+      "foo title",
+      "foo description",
+      200,
+      200
+    );
+
+    await simpleStorageInstance.createProperty(
+      "foo title",
+      "foo description",
+      200,
+      200,
+      { from: accounts[1] }
+    );
+
+    const numOfLessorExpected = 2;
+
+    const lessors = await simpleStorageInstance.getLessors.call();
+
+    assert.equal(
+      lessors.length,
+      numOfLessorExpected,
+      "The value 89 was not stored."
+    );
+  });
+
+  it("...should create property correcly ", async () => {
+    const simpleStorageInstance = await RentProperty.deployed();
+
+    await simpleStorageInstance.createProperty(
+      "foo title",
+      "foo description",
+      200,
+      200
+    );
+
+    const lessors = await simpleStorageInstance.getLessors.call();
+    const {
+      title,
+      description,
+      montlyPrice,
+      depositPrice,
+      lessor,
+      tenant,
+      available,
+    } = lessors[0].properties[0];
+
+    assert.equal(title, "foo title", "title was no stored correcly.");
+    assert.equal(
+      description,
+      "foo description",
+      "description was no stored correcly."
+    );
+    assert.equal(montlyPrice, "200", "montlyPrice was no stored correcly.");
+    assert.equal(depositPrice, "200", "depositPrice was no stored correcly.");
+    assert.equal(
+      tenant,
+      "0x0000000000000000000000000000000000000000",
+      "tenant was no stored correcly."
+    );
+
+    assert.equal(available, true, "available was no stored correcly.");
+    assert.equal(lessor, accounts[0], "tenant was no stored correcly.");
+  });
+
+  it("...should rent property correcly", async () => {
+    const simpleStorageInstance = await RentProperty.deployed();
+
+    await simpleStorageInstance.createProperty(
+      "foo title",
+      "foo description",
+      200,
+      200,
+      { from: accounts[0] }
+    );
+    // value should be the sum of depositPrice and montlyPrice
+    await simpleStorageInstance.rentProperty(accounts[0], 0, {
+      value: 400,
+      from: accounts[1],
+    });
+    const lessors = await simpleStorageInstance.getLessors.call();
+    const { depositAmount, tenant, available } = lessors[0].properties[0];
+    assert.equal(tenant, accounts[1], "tenant was no stored correcly.");
+    assert.equal(available, false, "tenant was no stored correcly.");
+    assert.equal(depositAmount, "200", "tenant was no stored correcly.");
+  });
+
+  it("...should get lessor correctly", async () => {
+    const simpleStorageInstance = await RentProperty.deployed();
+
+    await simpleStorageInstance.createProperty(
+      "foo title",
+      "foo description",
+      200,
+      200
+    );
+
+    const lessors = await simpleStorageInstance.getLessors.call();
+    assert.equal(lessors.length, 2, "lessors was no stored correcly.");
   });
 });
