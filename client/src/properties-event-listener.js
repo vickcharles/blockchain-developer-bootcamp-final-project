579a57dd53dcce168026aba1874c42950321b4c0
Vickler <charlesvikler@gmail.com>
2021-12-01 00:26:14 +0100

    more changes

--- client/src/properties-event-listener.js
+++ client/src/properties-event-listener.js
@@ -1,15 +1,11 @@
 import RealEstateProperty from "../src/services/RealEstateProperty";
 import { getAllProperties } from "./redux/Properties";
 
-const PropertyEvents = async (dispatch, getState) => {
-  const state = getState();
-
-  if (state.app.generalError) {
-    const instance = await RealEstateProperty();
-    await instance.events.PropertyCreated((error, candidateList) => {
-      dispatch(getAllProperties());
-    });
-  }
+const PropertyEvents = async (dispatch) => {
+  const instance = await RealEstateProperty();
+  await instance.events.PropertyCreated(() => {
+    dispatch(getAllProperties());
+  });
 };
 
 export default PropertyEvents;
